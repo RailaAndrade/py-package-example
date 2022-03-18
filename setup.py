125c36b1bce668131b775f11e18fe2a52e24f15a
railaandrade <raila_andrade@hotmail.com>
2022-03-18 18:55:13 -0300

    mudanca nome do pacote

--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package test",
+    name="package-dio-test",
     version="0.0.1",
     author="raila",
     description="test publication python package",
     long_description=page_description,
     long_description_content_type="text/markdown",
     url="https://github.com/RailaAndrade/py-package-example",
-    
-    packages=find_packages('package test'),
+    packages=find_packages('package-dio-test'),
     install_requires=requirements,
     python_requires='>=3.8',
 )
